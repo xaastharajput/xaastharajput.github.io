9d8cd048940d342000367ebd40fbecdd20712642
aastha <xaastha.rajput@gmail.com>
2017-04-19 01:34:51 +0530

    first commit

---
+++ js/grayscale.js
@@ -0,0 +1,45 @@
+/*!
+ * Author : AasthaRajput.Com
+ * Start Bootstrap (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+ 
+// NavBar Push Website down
+var toggle = 0;
+$('.navbar-toggle').click(function() {
+  if(toggle==0)
+  {
+  	$(".intro").css('margin-top','215px');
+  	toggle=1;
+  }
+  else
+  {
+  	$(".intro").css('margin-top','0px');
+  	toggle=0;
+  }
+});
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
